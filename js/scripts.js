8102684792ef09985c3ac765fdd36cbaf0ceb8cb
Liza Rozenberger <126602412+lizarozenberger@users.noreply.github.com>
2023-05-26 22:03:24 +0400

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".nav_list"),
+        hamburger = document.querySelector(".hamburger");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("hamburger_active");
+        menu.classList.toggle("menu_active");
+    });
+});
+
 var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
@@ -9,3 +19,15 @@ var swiper = new Swiper(".mySwiper", {
 new WOW({
     scrollContainer: null,
 }).init();
+
+const items = document.querySelectorAll(".item");
+const item1Img = document.querySelector(".item1 img");
+
+items.forEach((item) => {
+    item.addEventListener("click", () => {
+        const itemImg = item.querySelector("img");
+        let imgSrc = itemImg.src;
+        itemImg.src = item1Img.src;
+        item1Img.src = imgSrc;
+    });
+});
